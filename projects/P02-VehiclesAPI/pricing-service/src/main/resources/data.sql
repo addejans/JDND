b59ef70357610c94291e3553d3e921d914157fe8
addejans <adam.dejans@gmail.com>
2020-12-30 10:53:36 -0500

    added data.sql and pricing service tests (not passing)

---
+++ projects/P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into price (vehicle_Id, currency, price) values (1,'USD', 10000.01);
+insert into price (vehicle_Id, currency, price) values (2,'USD', 20000.02);
+insert into price (vehicle_Id, currency, price) values (3,'USD', 30000.03);
+insert into price (vehicle_Id, currency, price) values (4,'USD', 40000.04);
+insert into price (vehicle_Id, currency, price) values (5,'USD', 50000.05);
--- projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,82 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.test.web.servlet.MockMvc;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	String BASE_URL = "http://localhost:";
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void testGetPriceById() {
+		ResponseEntity<Price> response =
+				this.restTemplate.getForEntity(BASE_URL + port + "/price/1", Price.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		assertThat(response.getBody().getPrice().intValue(), equalTo(10000));
+	}
+
+	@Test
+	public void testGetAllPrices() {
+		ResponseEntity<List> response =
+				restTemplate.getForEntity(BASE_URL + port + "/prices", List.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+	}
+
+
+
+	//TODO: Check this out later
+//	@Test
+//	public void getPrice() throws Exception {
+//		mockMvc.perform(get("/services/price/?vehicleId=1"))
+//				.andExpect(status().isOk())
+//				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+//				.andExpect(content().json("{}"));
+//	}
+//
+//	@Test
+//	public void testInitializedRepositorySampleData() {
+//		Iterable<Price> prices = priceRepository.findAll();
+//		assertThat(prices, is(iterableWithSize(5)));
+//		assertNotNull(prices.iterator().next().getId());
+//	}
+
+
+
+
 
 }
